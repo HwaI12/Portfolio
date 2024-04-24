aa0bb48c002af27d21274f1ae4ee87b755d7f84e
HwaI12 <s2222061@stu.musashino-u.ac.jp>
2024-04-24 23:19:48 +0900

    worksの追加

--- src/components/Pages/Work/koijan.js
+++ src/components/Pages/Work/koijan.js
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import githubLogo from '../../../Images/icon_image/github-logo_icon-icons.com_73546.png';
 
-const koijan = () => {
+const Koijan = () => {
+  const [fadeIn, setFadeIn] = useState(false);
+
+  useEffect(() => {
+    setFadeIn(true);
+    document.body.classList.add('scroll-to-top');
+    window.scrollTo(0, 0);
+    setTimeout(() => {
+      document.body.classList.remove('scroll-to-top');
+    }, 1000);
+  }, []);
+
+  const githubUrl = "https://github.com/HwaI12/koi-jan";
+
+  const handleGithubClick = () => {
+    window.open(githubUrl, "_blank");
+  };
+
   return (
-    <div className="container text-center">
-      <div>hello</div>
+    <div className={`container text-center ${fadeIn ? "fade-in" : ""}`}>
+      <section className='Top'>
+        <h2 className={`text-uppercase ${fadeIn ? "fade-in" : ""}`}>恋じゃん</h2>
+        <div className={`g-slide ${fadeIn ? "fade-in" : ""}`} style={{ maxWidth: "960px", width: "100%", maxHeight: "569px", height: "100%", margin: "0 auto" }}>
+          <iframe
+            title="Google Presentation"
+            src="https://docs.google.com/presentation/d/e/2PACX-1vRHkQVU8VvXyiUsWqIGROUxcIHuejJwTUnrzfr69e87YyMgbmtEq9vgzCfAvHGtti9688h2Rl7U88bQ/embed?start=false&loop=false&delayms=3000"
+            style={{ width: "100%", height: "100%" }}
+            allowFullScreen={true}
+            mozAllowFullScreen={true}
+            webkitAllowFullScreen={true}
+          ></iframe>
+        </div>
+        <div className={`github-link-logo ${fadeIn ? "fade-in" : ""}`} onClick={handleGithubClick}>
+          <img src={githubLogo} alt="GitHub" />
+        </div>
+      </section>
     </div>
   );
 };
 
-export default koijan;
+export default Koijan;
--- src/components/Pages/Work/mrhmdLFL.js
+++ src/components/Pages/Work/mrhmdLFL.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const mrhmdLFL = () => {
+const MrhmdLFL = () => {
+  const [fadeIn, setFadeIn] = useState(false);
+
+  useEffect(() => {
+    setFadeIn(true);
+    document.body.classList.add('scroll-to-top');
+    window.scrollTo(0, 0);
+    setTimeout(() => {
+      document.body.classList.remove('scroll-to-top');
+    }, 1000);
+  }, []);
+
   return (
-    <div className="container text-center">
-      <div>hello</div>
+    <div className={`container text-center ${fadeIn ? "fade-in" : ""}`}>
+      <section className='Top'>
+        <h2 className={`text-uppercase ${fadeIn ? "fade-in" : ""}`} style={{ margin: "0 0 20px 0" }}>MRHMDと物体認識を活用した日常環境における自然な外国語学習手法の提案</h2>
+        <div className={`${fadeIn ? "fade-in" : ""}`} style={{ maxWidth: "960px", width: "100%" }}>
+          <div style={{ position: "relative", width: "100%", paddingBottom: "59.27%", boxShadow: "0 2px 8px 0 rgba(63,69,81,0.16)", borderRadius: "8px", overflow: "hidden" }}>
+            <iframe title="Canva Slide"
+              loading="lazy"
+              style={{ position: "absolute", width: "100%", height: "100%", top: 0, left: 0, border: "none", padding: 0, margin: 0 }}
+              src="https://www.canva.com/design/DAF-n0YZATY/BEJ1hlv_T3ak8Zz33RhAcw/view?embed"
+              allowFullScreen={true}
+              allow="fullscreen">
+            </iframe>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
 
-export default mrhmdLFL;
+export default MrhmdLFL;
