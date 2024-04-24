359690640e01039982e5d56a249ccd4a2bbf18ae
HwaI12 <s2222061@stu.musashino-u.ac.jp>
2024-04-25 01:08:54 +0900

    タイトルとプロフィールの名前を修正

--- src/components/Pages/Top/TopProfile.js
+++ src/components/Pages/Top/TopProfile.js
@@ -5,7 +5,7 @@ const TopProfile = () => {
   return (
     <section className="Top" id="Topprofile">
       <img src={profileImage} className="profileImage" alt="" />
-      <h2>Ishikawa Fami</h2>
+      <h2>Fami Ishikawa</h2>
       <p>Faculty of Data Science, Musashino University</p>
     </section>
   );
