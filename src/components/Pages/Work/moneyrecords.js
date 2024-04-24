d4cc0a17159916ac9c1952c7adad57ce6446479e
HwaI12 <s2222061@stu.musashino-u.ac.jp>
2024-04-25 00:08:08 +0900

    ポートフォリオのパス修正とOrderStreamコンポーネントの追加

--- src/components/Pages/Work/moneyrecords.js
+++ src/components/Pages/Work/moneyrecords.js
@@ -1,11 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import githubLogo from '../../../Images/icon_image/github-logo_icon-icons.com_73546.png';
 
-const moneyrecords = () => {
+const MoneyRecords = () => {
+  const [fadeIn, setFadeIn] = useState(false);
+
+  useEffect(() => {
+    setFadeIn(true);
+    document.body.classList.add('scroll-to-top');
+    window.scrollTo(0, 0);
+    setTimeout(() => {
+      document.body.classList.remove('scroll-to-top');
+    }, 1000);
+  }, []);
+
+  const githubUrl = "https://github.com/HwaI12/monthly-hackathon-23-vol5";
+
+  const handleGithubClick = () => {
+    window.open(githubUrl, "_blank");
+  };
+
   return (
-    <div className="container text-center">
-      <div>hello</div>
+    <div className={`container text-center ${fadeIn ? "fade-in" : ""}`}>
+      <section className='Top'>
+        <h2 className={`text-uppercase ${fadeIn ? "fade-in" : ""}`}>MONEY RECORDS</h2>
+        <div className={`g-slide ${fadeIn ? "fade-in" : ""}`} style={{ maxWidth: "960px", width: "100%", margin: "20px auto" }}>
+          <div style={{ position: "relative", paddingBottom: "56.25%", width: "100%", height: 0 }}>
+            <iframe
+              title="Google Presentation"
+              src="https://docs.google.com/presentation/d/e/2PACX-1vT1cPfcdPs53kN4wG9Dp0yAfx3mVbGXwIQWY3HcHUr_5yFSRzVH3pGKM3a-ej3FC5PHpKGic_GoKZm-/embed?start=false&loop=false&delayms=3000"
+              style={{ position: "absolute", width: "100%", height: "100%", top: 0, left: 0 }}
+              allowFullScreen={true}
+              mozAllowFullScreen={true}
+              webkitAllowFullScreen={true}
+            ></iframe>
+          </div>
+        </div>
+        <div className={`github-link-logo ${fadeIn ? "fade-in" : ""}`} onClick={handleGithubClick}>
+          <img src={githubLogo} alt="GitHub" />
+        </div>
+      </section>
     </div>
   );
 };
 
-export default moneyrecords;
+export default MoneyRecords;
