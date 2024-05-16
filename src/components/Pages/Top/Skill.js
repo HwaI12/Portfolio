2cc271dbdb5001d15c2ff35e39e639ccd4b55221
HwaI12 <s2222061@stu.musashino-u.ac.jp>
2024-05-16 14:41:47 +0900

    Update image border radius and header font size

--- src/components/Pages/Top/Skill.js
+++ src/components/Pages/Top/Skill.js
@@ -5,10 +5,10 @@ import jsLogo from '../../../Images/icon_image/javascript_plain_logo_icon_146454
 import tsLogo from '../../../Images/icon_image/typescript_original_logo_icon_146317.png';
 import reactLogo from '../../../Images/icon_image/react_original_logo_icon_146374.png';
 import vueLogo from '../../../Images/icon_image/file_type_vue_icon_130078.png';
-import nextLogo from '../../../Images/icon_image/nextjs_icon_132160.png';
-import nodeLogo from '../../../Images/icon_image/nodejs_plain_logo_icon_146409.png';
+// import nextLogo from '../../../Images/icon_image/nextjs_icon_132160.png';
+// import nodeLogo from '../../../Images/icon_image/nodejs_plain_logo_icon_146409.png';
 import postgresLogo from '../../../Images/icon_image/postgresql_original_logo_icon_146391.png';
-import supabaseLogo from '../../../Images/icon_image/supabase_logo_icon_249481.png';
+// import supabaseLogo from '../../../Images/icon_image/supabase_logo_icon_249481.png';
 import htmlLogo from '../../../Images/icon_image/file_type_html_icon_130541.png';
 import cssLogo from '../../../Images/icon_image/file_type_css_icon_130661.png';
 import gitLogo from '../../../Images/icon_image/git_plain_logo_icon_146507.png';
@@ -44,10 +44,7 @@ const Skill = () => {
     { src: tsLogo, alt: 'TypeScript' },
     { src: reactLogo, alt: 'React' },
     { src: vueLogo, alt: 'Vue.js' },
-    { src: nextLogo, alt: 'Next.js' },
-    { src: nodeLogo, alt: 'Node.js' },
     { src: postgresLogo, alt: 'PostgreSQL' },
-    { src: supabaseLogo, alt: 'Supabase' },
     { src: htmlLogo, alt: 'HTML' },
     { src: cssLogo, alt: 'CSS' },
     { src: gitLogo, alt: 'Git' },
